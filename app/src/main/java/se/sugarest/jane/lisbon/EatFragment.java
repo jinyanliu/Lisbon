b37c7b482d069be2ef148fa26ad398be531ffc2b
jinyanliu <jane.jinyan.liu@gmail.com>
2017-12-22 16:27:22 +0100

    Add 1 restaurant.

--- app/src/main/java/se/sugarest/jane/lisbon/EatFragment.java
+++ app/src/main/java/se/sugarest/jane/lisbon/EatFragment.java
@@ -24,12 +24,13 @@ public class EatFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.place_list, container, false);
 
         //Create an array list of places.
-        ArrayList<Place> places = new ArrayList<Place>();
+        ArrayList<Place> places = new ArrayList<>();
 
+        places.add(new Place(R.string.eat_name_lamei_hot_pot, R.string.eat_sd_lamei_hot_pot, R.string.eat_web_lamei_hot_pot, R.drawable.lamei_hotpot));
         places.add(new Place(R.string.eat_name_grelha_do_carmo, R.string.eat_sd_grelha_do_carmo, R.string.eat_web_grelha_do_carmo, R.drawable.grelha_do_carmo));
         places.add(new Place(R.string.eat_name_time_out_market_lisboa, R.string.eat_sd_time_out_market_lisboa, R.string.eat_web_time_out_market_lisboa, R.drawable.time_out_market));
         places.add(new Place(R.string.eat_name_marisqueira_azul, R.string.eat_sd_marisqueira_azul, R.string.eat_web_marisqueira_azul, R.drawable.marisqueira_azul));
-        places.add(new Place(R.string.eat_name_lamei_hot_pot, R.string.eat_sd_lamei_hot_pot, R.string.eat_web_lamei_hot_pot, R.drawable.lamei_hotpot));
+        places.add(new Place(R.string.eat_name_portugalia_almirante_reis, R.string.eat_sd_portugalia_almirante_reis, R.string.eat_web_portugalia_almirante_reis, R.drawable.portugalia_almirante_reis));
 
         /**
          * Create a PlaceAdapter, whose data source is a list of Places.
